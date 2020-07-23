6cbb7689adf5bb558d5526638669d59b1196c8f0
Massimiliano Ranauro <huckbit@gmail.com>
2020-07-23 23:20:45 +0100

    some other stile on component

--- src/components/IpCard.js
+++ src/components/IpCard.js
@@ -8,9 +8,11 @@ export class IpCard extends Component {
     const { ip, region, country_name, country_code, isp } = this.props.data;
     if (this.props.loaded == null) {
       return (
-        <div className="columns wrapper">
-          <div className="column is-6 is-offset-3 has-text-centered">
-            <h1 className="is-size-3">Gathering information ...</h1>
+        <div className="container wrapper">
+          <div className="columns">
+            <div className="column is-6 is-offset-3 has-text-centered">
+              <h1 className="is-size-3">Gathering information ...</h1>
+            </div>
           </div>
         </div>
       );
@@ -19,26 +21,24 @@ export class IpCard extends Component {
       <div className="container wrapper">
         <div className="columns is-centered-mobile">
           <div className="column is-10-mobile is-offset-1-mobile is-6 is-offset-3 has-text-centered">
-            <div className="mt-5">
-              <h2 className="is-size-1 has-text-weight-bold">Your IP</h2>
-              <div className="notification mt-2">
-                <img src={ipLogo} alt="ip" width="60" />
-                <h1 className="is-size-3 has-text-weight-bold">{ip}</h1>
-                <p className="mt-5">
-                  <img className="inline-img" src={Location} alt="Location" /> {region} -
-                  {country_name}({country_code})
-                </p>
-                <p className="mt-5">
-                  <img className="inline-img" src={IspImage} alt="ispImage" /> {isp}
-                </p>
-              </div>
-              <button
-                className="button is-large is-dark mt-3"
-                onClick={() => this.props.handleClick()}
-              >
-                Refresh
-              </button>
+            <h2 className="is-size-1 has-text-weight-bold">Your IP</h2>
+            <div className="notification mt-2">
+              <img src={ipLogo} alt="ip" width="60" />
+              <h1 className="is-size-1 is-size-2-mobile has-text-weight-bold">{ip}</h1>
+              <p className="mt-5">
+                <img className="inline-img" src={Location} alt="Location" /> {region} -
+                {country_name}({country_code})
+              </p>
+              <p className="mt-5">
+                <img className="inline-img" src={IspImage} alt="ispImage" /> {isp}
+              </p>
             </div>
+            <button
+              className="button is-large is-dark mt-3"
+              onClick={() => this.props.handleClick()}
+            >
+              Refresh
+            </button>
           </div>
         </div>
       </div>
