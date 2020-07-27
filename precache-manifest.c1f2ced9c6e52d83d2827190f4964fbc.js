40c6b571fe9c47224709025df6c07223ec74ffc5
Massimiliano Ranauro <huckbit@gmail.com>
2020-07-27 11:44:41 +0100

    gh-pages

---
+++ precache-manifest.c1f2ced9c6e52d83d2827190f4964fbc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b769eb421b6dd0451fba6284ed04174c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "45d38cc3b9c52e93b8d6",
+    "url": "./static/css/main.3efa96a9.chunk.css"
+  },
+  {
+    "revision": "7a205f6fc7de3cce039b",
+    "url": "./static/js/2.f04a40cd.chunk.js"
+  },
+  {
+    "revision": "07783bd9e59dc70e7485d0d76e1e79d1",
+    "url": "./static/js/2.f04a40cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d38cc3b9c52e93b8d6",
+    "url": "./static/js/main.866a612f.chunk.js"
+  },
+  {
+    "revision": "5522a758c2fb9fc6ad3f",
+    "url": "./static/js/runtime-main.b8705a92.js"
+  },
+  {
+    "revision": "6ac048fc7813a16fa22137e748486530",
+    "url": "./static/media/access-point-network.6ac048fc.svg"
+  },
+  {
+    "revision": "fd6ddbf51a7545193aba43d1f35454f1",
+    "url": "./static/media/ipNetwork.fd6ddbf5.svg"
+  },
+  {
+    "revision": "9732d55dad0cf07e4499e2c4b58e46fc",
+    "url": "./static/media/mapMarker.9732d55d.svg"
+  }
+]);
